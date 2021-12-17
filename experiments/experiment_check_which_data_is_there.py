a18c9f0db76d14fcae62e98bc83508dd2a40343d
Robin Andre <uleci@student.kit.edu>
2021-12-17 14:31:46 +0100

    added the broken data to repo

--- experiments/experiment_check_which_data_is_there.py
+++ experiments/experiment_check_which_data_is_there.py
@@ -10,6 +10,8 @@ if __name__ == '__main__':
     expath = Path("/home/paincrash/Desktop/master-thesis/experiment_results_permanent/neural_network_data_modechoice")
     for path in expath.iterdir():
         yaml, data = simulation.load(str(path) + "/")
+        print(path)
+        print(metric.get_all_existing_modes(data.travel_time.data_frame))
         data.draw_distributions()
 
 
