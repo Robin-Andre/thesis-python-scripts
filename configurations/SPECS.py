475a883186f781882147ddf5591d032692e5d5d8
MalleusMalleficarum <robinandre1995@web.de>
2022-02-19 13:17:38 +0100

    fixed plotting to work with new data frames names

--- configurations/SPECS.py
+++ configurations/SPECS.py
@@ -2,7 +2,7 @@
 This file contains all the relevant machine information such as the installation path of mobitopp and
 other specifics
 """
-CWD = "C:\\Users\\bo5742\\mobitopp-example-rastatt\\"
+CWD = "C:/Users/Admin/Desktop/master-thesis/mobitopp-example-rastatt/"
 
 TEMP = "C:\\Users\\Admin\\Desktop\\master-thesis\\mobitopp-example-rastatt\\"
 EXP_PATH = "C:\\Users\\bo5742\\Desktop\\thesis-experiments"
