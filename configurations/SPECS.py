715ecee821b5f23a8df624cd7e113700af349211
Robin-Andre <uleci@student.kit.edu>
2022-03-30 15:18:56 +0200

    added specs because I added it to the repo again, ima dumbi

--- configurations/SPECS.py
+++ configurations/SPECS.py
@@ -2,8 +2,7 @@
 This file contains all the relevant machine information such as the installation path of mobitopp and
 other specifics
 """
-CWD = "C:/Users/Admin/Desktop/master-thesis/mobitopp-example-rastatt/"
-
+CWD = "C:/Users/bo5742/mobitopp-example-rastatt/"
 TEMP = "C:\\Users\\Admin\\Desktop\\master-thesis\\mobitopp-example-rastatt\\"
 EXP_PATH = "C:\\Users\\bo5742\\Desktop\\thesis-experiments\\"
 NUMPY = "C:\\Users\\Admin\\Desktop\\master-thesis\\numpy_results\\"
